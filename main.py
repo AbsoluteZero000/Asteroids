6eccb8510dd0ba45db5ee5e89505ed951f457a0b
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-17 18:35:08 +0300

    feature: added rotation

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.draw(screen)
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 
@@ -20,3 +20,12 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotation -= PLAYER_TURN_SPEED * dt
+
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_TURN_SPEED * dt
